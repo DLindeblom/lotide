3019ae147215a264d64f13287975cad73babacf8
DLindeblom <drdlindeblom@gmail.com>
2022-06-02 23:07:03 +0000

    in a better position now

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    
+    if (sentence[i] === " ") {
+      continue;
+    }
+    if (sentence[i] !== " " && results[sentence[i]] === undefined) {
+      (results[sentence[i]] = [i]);
+    } else {
+      results[sentence[i]].push(i);
+    }
+  }
+  
+  console.log(results);
+};
+
+letterPositions("lighthouse in the house");
+
+
