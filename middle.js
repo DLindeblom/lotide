965b7a1b5ce995ae72453c62049cd8179783c54a
DLindeblom <drdlindeblom@gmail.com>
2022-06-02 00:58:09 +0000

    no longer stuck in the middle

--- middle.js
+++ middle.js
@@ -9,7 +9,7 @@ const eqArrays = function(arr1, arr2) {
 const assertArraysEqual = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log("🔴🔴🔴 Assertion Failed: arr1 !== arr2")
+      console.log("🔴🔴🔴 Assertion Failed: arr1 !== arr2");
       return;
     }
   } console.log("✅✅✅ Assertion Passed: arr1 === arr2");
@@ -17,18 +17,18 @@ const assertArraysEqual = function(arr1, arr2) {
 
 const middle = function(array) {
   let newArray = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array.length < 3) {
-      return newArray;
-    } else if (array.length % 2 !== 0) {
-        newArray = (array[Math.floor(array.length/2)]);
-    } else if (array.length % 2 === 0) {
-        newArray = (array[(array.length/2) -1] array[(array.length/2)]) ;
-    }
+  if (array.length < 3) {
+    return newArray;
+  }
+  if (array.length % 2 !== 0) {
+    newArray.push(array[Math.floor(array.length / 2)]);
+  } else if (array.length % 2 === 0) {
+    newArray.push((array[Math.floor(array.length / 2) - 1]));
+    newArray.push((array[Math.floor(array.length / 2)]));
   } return newArray;
-}
+};
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1,3,5]));
-console.log(middle([1,2,4,6,7,8]));
+assertArraysEqual(middle([1]), {}); // => []
+assertArraysEqual(middle([1, 2], {})); // => []
+assertArraysEqual(middle([1,3,5]), [3]);
+assertArraysEqual(middle([1,3,5,7,9,11,13,15,17,19]), [9, 11]);
