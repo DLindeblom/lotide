20291565748b6c5d3c9f83671313a15bf85e8cbf
DLindeblom <drdlindeblom@gmail.com>
2022-06-03 00:19:19 +0000

    a very valuable key

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  const passed = `✅✅✅Assertion Passed: ${actual} === ${expected}`;
+  const failed = `🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(passed);
+  } else {
+    console.log(failed);
+  }
+};
+
+
+const findKeyByValue = function(object, value) {
+  
+  for (x in object) {
+    if (object[x] === value) {
+      return x;
+    }
+  }  
+}
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
